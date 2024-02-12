3ca9835163392cfc8d31f2ca3af3d1e0569e2594
ThiagoLuzFerreira <tlrferreira83@gmail.com>
2024-02-12 18:24:44 -0300

    config server configuration added

--- src/main/java/com/thiago/demoproject/Application.java
+++ src/main/java/com/thiago/demoproject/Application.java
@@ -2,8 +2,10 @@ package com.thiago.demoproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableConfigServer
 @EnableFeignClients
 @SpringBootApplication
 public class Application {
